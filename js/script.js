cb865b45828ea85b1c494e2bb2bec383c5f028f5
Manikanta L.S.E <manikanta.lse@gmail.com>
2018-12-13 10:27:13 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 54
+  });
+
+  $('.topbar').hide();
+
+  $('.menu-icon').click(function () {
+    $('.topbar').toggle();
+    $('.top-row').toggleClass('top-row-bg');
+  });
+
+});
